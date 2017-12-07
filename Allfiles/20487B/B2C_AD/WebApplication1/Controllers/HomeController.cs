c07df987fe1578e97faaa7bf4d1047a99915259a
Joshua <joshua.anthony@habanero.se>
2017-12-07 13:09:28 -0800

    Added b2c / app service google authentication claims based site.

---
+++ Allfiles/20487B/B2C_AD/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return Redirect("/.auth/logout");
+        }
+
+        public ActionResult LoginAd()
+        {
+            return Redirect("/.auth/login/aad");
+        }
+
+        public ActionResult LoginGoogle()
+        {
+            return Redirect("/.auth/login/google");
+        }
+
+        public ActionResult EditProfile()
+        {
+            return Redirect("{B2CEditProfilePolicyUrl}");
+        }
+    }
+}
